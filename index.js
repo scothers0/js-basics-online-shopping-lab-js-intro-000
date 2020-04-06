4d7ec20a98c8f9807770e349d91a008e0030f863
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-04-06 17:29:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ return cart;
 
 function placeOrder(cardNumber) {
   if (cardNumber != undefined){
-    let cart = [];
+    cart.splice(0, cart.length);
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
   return "Sorry, we don't have a credit card on file for you.";
