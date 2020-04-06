4add42c37f59b420cf319e98d7504f6ab623d834
harmonious-cache-6099 <isaac_scothern@hotmail.co.uk>
2020-04-06 18:01:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function removeFromCart(item) {
 
   for (let i = 0; i < cart.length; i++) {
     if (item == cart[i].itemName) {
-      cart.splice(i,1);
+      cart.splice(i, 1);
       return cart;
     }
   }
